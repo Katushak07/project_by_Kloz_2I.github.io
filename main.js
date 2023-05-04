131e6c64a159288a5cc19c62ed27e7012c2d6b37
Katushak07 <yekaterinaklez@gmail.com>
2023-05-04 08:55:16 +0300

    f

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
-$(".navbar link").click(function(){
-    $("body,html").animate({
-    scrollTop:$("#" + $(this).data('value')).offset().top
-    },1000)
-    
-    })
+const links = document.querySelectorAll('.link');
+const sections = document.querySelectorAll('section');
 
-    $(document).ready(function(){
-        $('.header').height($(window).height());
-        })
+let activeLink = 0;
+
+links.forEach((link, i) => {
+    link.addEventListener('click', () => {
+        if(activeLink != i){
+            links[activeLink].classList.remove('active');
+            link.classList.add('active');
+            sections[activeLink].classList.remove('active');
+
+            setTimeout(() => {
+                activeLink = i;
+                sections[i].classList.add('active');
+            }, 1000);
+        }
+    })
+})
